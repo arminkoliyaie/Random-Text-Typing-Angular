d34750f688efc1b1c8b577d867f51f816f2bcaa5
arminkoliyaie <akoliaye@gmail.com>
2022-03-16 14:01:16 +0330

    commuting form PC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { getLocaleEraNames } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Typing';
+  randomText: string =
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, deleniti.';
+  inputText: string = '';
+
+  onChangeInput(text: string) {
+    this.inputText = text;
+  }
+  compare(letterRandom:string , letterEnter:string):string{
+   if(!letterEnter){
+     return 'pending';
+   }
+   return letterEnter === letterRandom ? 'correct' : 'incorrect';
+  }
 }
